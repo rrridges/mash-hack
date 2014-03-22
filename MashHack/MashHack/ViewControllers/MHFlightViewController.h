ab004a96a1b25c614e6d3ee52f3d786fcb2e2758
Matt Bridges <matt@intrepid.io>
2014-03-22 12:18:10 -0400

    Add flight view controller

---
+++ MashHack/MashHack/ViewControllers/MHFlightViewController.h
@@ -0,0 +1,13 @@
+//
+//  MHFlightViewController.h
+//  MashHack
+//
+//  Created by Matt Bridges on 3/22/14.
+//  Copyright (c) 2014 Intrepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MHFlightViewController : UIViewController
+
+@end
